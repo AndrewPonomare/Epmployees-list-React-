055c2fc24c5caeeba5427b4dcf9dbf59adcd4ae5
Andrii <Andrewponomare@gmail.com>
2022-06-30 23:36:54 +0300

    edit components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,5 @@
 
+import AppFilter from "../app-filter/app-filter";
 import AppInfo from "../app-info/app-info";
 import SearchPanel from "../search-panel/search-panel";
 import "./app.css"
@@ -10,6 +11,7 @@ function App() {
 
             <div className="search-panel">
                 <SearchPanel />
+                <AppFilter />
             </div>
         </div>
     );
